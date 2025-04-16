917b860b5442a98f4f4cc67ea747bf0455ec60b1
Marjorie06 <marjoriechal06@gmail.com>
2025-04-16 11:21:56 -0500

    Subir

---
+++ src/main/java/com/mycompany/cumpleanosfeliz/CumpleanosFeliz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cumpleanosfeliz;
+
+/**
+ *
+ * @author IDC
+ */
+public class CumpleanosFeliz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
